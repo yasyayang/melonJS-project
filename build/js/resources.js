921d3ca4c9ddf9965507dd8fae05eeb91faf3210
YASYA <cnchachazone7@gmail.com>
2018-10-23 00:12:14 +0700

    Add files via upload

---
+++ build/js/resources.js
@@ -0,0 +1,27 @@
+game.resources = [
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "metatiles32x32",
+        "type": "image",
+        "src": "data/img/map/metatiles32x32.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "ground",
+        "type": "tmx",
+        "src": "data/map/ground.tmx"
+    }
+];
